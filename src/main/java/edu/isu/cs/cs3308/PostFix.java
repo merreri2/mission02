8e0057d6774c4835e98f4f65daf3014f46c65bf9
Erik Merrill <merreri2@isu.edu>
2019-01-25 01:19:28 -0700

    added postFix file

--- src/main/java/edu/isu/cs/cs3308/PostFix.java
+++ src/main/java/edu/isu/cs/cs3308/PostFix.java
@@ -1,5 +1,9 @@
 package edu.isu.cs.cs3308;
 
+import edu.isu.cs.cs3308.structures.impl.LinkedStack;
+
+import static java.lang.Integer.parseInt;
+
 /**
  * Postfix expression evaluator.
  *
@@ -18,6 +22,70 @@ public class PostFix {
      * empty, or contains only whitespace.
      */
     public static int evalPostFix(String infix) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //throw new UnsupportedOperationException("Not supported yet.");
+        //Create an empty stack
+        LinkedStack<Integer> stack = new LinkedStack<Integer>();
+        //if string is null return 0
+        if (infix == null || infix.trim().isEmpty()){
+            return 0;
+        }
+        else {
+            //Create a string array to hold infix split by ' '
+            String [] tempArray = infix.split(" ");
+            int temp = 0;
+            //Iterate through the tempArray
+            for(int count = 0; count < tempArray.length; count++){
+                //check if the string at index count is equal to one of the arithmetic operators
+                //if it isn't we know the contents of the string is an integer, so we parse it to an int and push it onto the stack
+                if (!tempArray[count].equals("+") && !tempArray[count].equals("-") && !tempArray[count].equals("*") && !tempArray[count].equals("/")){
+                    stack.push(parseInt(tempArray[count]));
+                }
+                else if (tempArray[count].equals("+")){
+                    isNull(stack.peek());
+                    int temp1 = stack.pop();
+                    isNull(stack.peek());
+                    int temp2 = stack.pop();
+                    temp = temp2 + temp1;
+                    stack.push(temp);
+                }
+                else if (tempArray[count].equals("-")){
+                    isNull(stack.peek());
+                    int temp1 = stack.pop();
+                    isNull(stack.peek());
+                    int temp2 = stack.pop();
+                    temp = temp2 - temp1;
+                    stack.push(temp);
+                }
+                else if (tempArray[count].equals("*")){
+                    isNull(stack.peek());
+                    int temp1 = stack.pop();
+                    isNull(stack.peek());
+                    int temp2 = stack.pop();
+                    temp = temp2 * temp1;
+                    stack.push(temp);
+                }
+                else if (tempArray[count].equals("/")){
+                    isNull(stack.peek());
+                    int temp1 = stack.pop();
+                    isNull(stack.peek());
+                    int temp2 = stack.pop();
+                    temp = temp2 / temp1;
+                    stack.push(temp);
+                }
+
+                //write another method to check each int poping off the stack to make sure the stack has an int on it, if not, throw the required exception
+
+            }
+            return stack.peek();
+        }
+
+
     }
-}
+
+    private static void isNull(Integer toCheck) throws IllegalArgumentException{
+        if (toCheck == null){
+            throw new IllegalArgumentException();
+        }
+    }
+
+        }
